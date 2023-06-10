d4de993b2b7209a26b0f10487ee52456bf1e0d58
Marcus <marcus.guerra@ufv.br>
2023-06-10 18:13:21 -0300

    uptade csv

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def limpaSemPalavra(palavraChave, tweet, horario):
     return horario, tweet
 
 
-palavraChave = "Reserva Indígena"
+palavraChave = "garimpo"
 normalized_word = unicodedata.normalize("NFD", palavraChave)
 
 # Remove non-spacing marks and convert to lowercase
