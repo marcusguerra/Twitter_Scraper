48b56d0623d501c17f8d699d8cc0861b01413180
Marcus <marcus.guerra@ufv.br>
2023-06-12 22:11:13 -0300

    uptade csv

--- dadaShow.py
+++ dadaShow.py
@@ -1,9 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import csv
 
-df = pd.read_csv('dados.csv', delimiter=',')
+# Specify the path to the CSV file
+csv_file = 'dados.csv'
 
-print(df.columns)
-keyword_counts = df['keyWord'].value_counts()
-print(keyword_counts)
-print("total = ", keyword_counts.sum())
+# Specify the word you want to remove
+word_to_remove = 'imagem'
+
+# Read the CSV file and remove the word
+with open(csv_file, 'r') as file:
+    csv_reader = csv.reader(file)
+    data = list(csv_reader)
+    for row in data:
+        if len(row) > 0:  # Check if the row is not empty
+            row[0] = row[0].replace(word_to_remove, '')  # Modify the 'tweet' column
+
+# Overwrite the CSV file with the updated data
+with open(csv_file, 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
