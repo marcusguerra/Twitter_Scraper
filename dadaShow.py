c0f05c946b5f1e528d59baf5a4d39ed00f49cd50
Marcus <marcus.guerra@ufv.br>
2023-06-17 18:50:28 -0300

    uptade csv

--- dadaShow.py
+++ dadaShow.py
@@ -7,7 +7,4 @@ print(df.columns)
 keyword_counts = df['keyWord'].value_counts()
 print(keyword_counts)
 print("total = ", keyword_counts.sum())
-tweet_value = df.loc[13392, 'tweet']
 
-# Print the value
-print(tweet_value)
