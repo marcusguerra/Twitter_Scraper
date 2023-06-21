9d817ec941dae86baa344d2c33c4546ee9a5ae99
Marcus <marcus.guerra@ufv.br>
2023-06-20 23:30:19 -0300

    uptade csv

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def limpaSemPalavra(palavraChave, tweet, horario):
     return horario, tweet
 
 
-palavraChave = "incragyukk"
+palavraChave = "MST111"
 normalized_word = unicodedata.normalize("NFD", palavraChave)
 palavraChave = "".join(
     char.lower() for char in normalized_word if not unicodedata.combining(char)
